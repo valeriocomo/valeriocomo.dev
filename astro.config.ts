fdfe2736ea5f9701b5caaf771970e563832fbdf0
Valerio Como <valeriocomo@gmail.com>
2024-12-12 18:31:10 +0000

    builds: build ok

--- astro.config.ts
+++ astro.config.ts
@@ -2,23 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
-// export default defineConfig({
-//   // used to generate images
-//   site:
-//     process.env.VERCEL_ENV === 'production'
-//       ? 'https://brutal.elian.codes/'
-//       : process.env.VERCEL_URL
-//       ? `https://${process.env.VERCEL_URL}/`
-//       : 'https://localhost:3000/',
-//   trailingSlash: 'ignore',
-//   integrations: [sitemap(), UnoCSS({ injectReset: true })],
-//   vite: {
-//     optimizeDeps: {
-//       exclude: ['@resvg/resvg-js'],
-//     },
-//   },
-// });
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
