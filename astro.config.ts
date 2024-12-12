89389f7de8dd05231ad4307450f8625f3848bf86
Valerio Como <valeriocomo@gmail.com>
2024-12-12 17:42:18 +0000

    builds: deploy astro demo

--- astro.config.ts
+++ astro.config.ts
@@ -2,19 +2,39 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
+// export default defineConfig({
+//   // used to generate images
+//   site:
+//     process.env.VERCEL_ENV === 'production'
+//       ? 'https://brutal.elian.codes/'
+//       : process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}/`
+//       : 'https://localhost:3000/',
+//   trailingSlash: 'ignore',
+//   integrations: [sitemap(), UnoCSS({ injectReset: true })],
+//   vite: {
+//     optimizeDeps: {
+//       exclude: ['@resvg/resvg-js'],
+//     },
+//   },
+// });
+
+const isProduction = process.env.ENVIRONMENT === 'production';
+
 export default defineConfig({
-  // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  // site: 'https://valeriocomo.github.io',
+  // base: 'valeriocomo.dev',
+  site: isProduction  ? 'https://valeriocomo.github.io' : 'https://localhost:3000/',
+  base: isProduction ?  'valeriocomo.dev' : '',
+  server: {
+    host: "127.0.0.1",
+    port: 4321
+  },
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
-  },
-});
+  },  
+})
