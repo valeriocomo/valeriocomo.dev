684280ce678be9455db3ab9dfe4256aaff326866
Valerio Como <valeriocomo@gmail.com>
2026-04-07 08:13:19 +0000

    feat: update OG image title to reflect site name

--- src/pages/v1/generate/og/[slug].png.ts
+++ src/pages/v1/generate/og/[slug].png.ts
@@ -30,7 +30,7 @@ export const GET: APIRoute = async ({ params, props }) => {
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; justify-content: space-between;">
           <div style="display: flex; flex-direction: column; gap: 0.75rem;">
-            <p style="font-size: 48px;">Brutal theme for Astro</p>
+            <p style="font-size: 48px;">valeriocomo.dev</p>
             <p style="font-size: 38px;">${title}</p>
           </div>
           <img src="https://valeriocomo.dev/_astro/me.Bk6xvcIs_1ME4QF.webp" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
