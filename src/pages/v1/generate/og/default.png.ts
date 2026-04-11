6d7cc502e126577d2ce048386e10d8731bcde174
Valerio Como <valeriocomo@gmail.com>
2026-04-11 16:19:59 +0000

    fix: fixed og

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -25,7 +25,7 @@ export const GET: APIRoute = async () => {
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="https://valeriocomo.dev/_astro/me.Bk6xvcIs_1ME4QF.webp" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://valeriocomo.dev/_astro/me.Cu4CtHao.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
